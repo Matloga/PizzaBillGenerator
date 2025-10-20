4b14d1e43b18048401e14a2d7383ab22e8828f5e
Matloga <tsumbedzomatloga@gmail.com>
2025-10-20 09:46:09 +0200

    Updated project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
+//        Pizza basePizza = new Pizza(true);
+//        basePizza.addExtraToppings();
+//        basePizza.addExtraCheese();
+//        basePizza.getBill();
+        DeluxPizza dp = new DeluxPizza(true);
+        dp.tkeAway();
+        dp.getBill();
     }
 }
--- src/Pizza.java
+++ src/Pizza.java
@@ -1,4 +1,56 @@
 public class Pizza {
-    private String price;
+    private int price;
     private Boolean veg;
+
+    private int extraCheesePrice = 100;
+    private int extraToppingPrice = 150;
+    private int backPackPrice = 20;
+    private int basePizzaPrice;
+
+    private boolean isExtraCheeseAdded = false;
+    private boolean isExtraToppingAdded = false;
+    private boolean isOptedForTakeAway = false;
+
+    public Pizza(Boolean veg) {
+        this.veg = veg;
+        if(this.veg) {
+            this.price = 300;
+        } else {
+            this.price = 400;
+        }
+        basePizzaPrice = this.price;
+    }
+
+    public void  addExtraCheese() {
+        isExtraCheeseAdded = true;
+        this.price += extraCheesePrice;
+    }
+
+    public void addExtraToppings() {
+        isExtraToppingAdded = true;
+        this.price += extraToppingPrice;
+    }
+
+    public  void tkeAway() {
+        isOptedForTakeAway = true;
+        this.price += backPackPrice;
+    }
+
+    public void getBill() {
+        String bill = "";
+        System.out.println("Pizza: " +basePizzaPrice);
+
+        if(isExtraCheeseAdded) {
+            bill += "Extra cheese added: "+extraCheesePrice+ "\n";
+        }
+        if(isExtraToppingAdded) {
+            bill += "Extra topping added: "+extraToppingPrice+ "\n";
+        }
+        if(isOptedForTakeAway) {
+            bill += "Take away: "+backPackPrice+ "\n";
+        }
+        bill += "Bill: "+this.price + "\n";
+        System.out.println(bill);
+    }
 }
+ 
